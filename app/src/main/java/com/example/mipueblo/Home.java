4d89233dd30f9862dc7f70aabcb81154e4f55a00
stevenarango <stcajugl@gmail.com>
2023-05-19 20:35:41 -0500

    se crea splash

---
+++ app/src/main/java/com/example/mipueblo/Home.java
@@ -0,0 +1,19 @@
+package com.example.mipueblo;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    }
+}
--- app/src/main/java/com/example/mipueblo/MainActivity.java
+++ app/src/main/java/com/example/mipueblo/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.mipueblo;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    // Se va setear los atributos o variables
+    MediaPlayer cancion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cancion = MediaPlayer.create(this, R.raw.audiio1);
+        cancion.start();
+
+        // temporizar la cancion:
+        TimerTask inicioApp = new TimerTask() {
+            @Override
+            public void run() {
+
+                //Intent intent = new Intent(MainActivity.this, Home.class);
+                Intent intent = new Intent(MainActivity.this,Home.class);
+                startActivity(intent);
+            }
+        };
+
+        Timer tiempo = new Timer();
+        tiempo.schedule(inicioApp, 15000);
+
     }
 }
