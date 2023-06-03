94b509bb6a01a1cb09e2e312d8f3d22be8d54e40
stevenarango <stcajugl@gmail.com>
2023-06-02 19:59:00 -0500

    se crean botones de arranque

--- app/src/main/java/com/example/mipueblo/Home.java
+++ app/src/main/java/com/example/mipueblo/Home.java
@@ -4,16 +4,55 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Home extends AppCompatActivity {
 
+    // ZONA DE ATRIBUTOS
+
+    Button botonHoteles;
+    Button botonRestaurantes;
+    Button botonSitiosTuristicos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        botonHoteles = findViewById(R.id.botonhoteles);
+        botonRestaurantes = findViewById(R.id.botonrestaurantes);
+        botonSitiosTuristicos = findViewById(R.id.botonturismo);
+
+        // escuchando los eventos de click en el boton
+        botonHoteles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Abrir nueva actividad
+                Intent intent = new Intent(Home.this, Hoteles.class);
+                startActivity(intent);
+
+            }
+        });
+
+        botonRestaurantes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 = new Intent(Home.this, Restaurantes.class);
+                startActivity(intent2);
+            }
+        });
+
+        botonSitiosTuristicos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3 = new Intent(Home.this, Sitios.class);
+                startActivity(intent3);
+            }
+        });
     }
 }
