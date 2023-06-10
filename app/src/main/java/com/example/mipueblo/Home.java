51204a8da10710b7a5deba7b0a3b4d63ce4a4330
OrionAlzate <orion_alzate@hotmail.com>
2023-06-09 20:50:26 -0500

    se crea menu de opcciones

--- app/src/main/java/com/example/mipueblo/Home.java
+++ app/src/main/java/com/example/mipueblo/Home.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -55,4 +57,37 @@ public class Home extends AppCompatActivity {
             }
         });
     }
+
+    // cargamos el menu de opciones
+    public boolean onCreateOptionsMenu(Menu menu){
+
+     getMenuInflater().inflate(R.menu.menu, menu);
+     return true;
+    }
+
+    // Funcion para cargar el funcionamento del menu
+    public boolean onOptionsItemSelected(MenuItem item){
+        // observar el comportamiento del ususario en el menu
+        int itemSeleccionado = item.getItemId();
+
+        switch (itemSeleccionado){
+            case (R.id.opcion1):
+                Toast.makeText(this, "accionaste Español", Toast.LENGTH_SHORT).show();
+                break;
+
+            case (R.id.opcion2):
+                Toast.makeText(this, "Accionaste Ingles", Toast.LENGTH_SHORT).show();
+                break;
+
+            case (R.id.opcion3):
+                Toast.makeText(this, "Accionaste Italiano", Toast.LENGTH_SHORT).show();
+                break;
+
+            case (R.id.opcion4):
+                Toast.makeText(this, "Accionaste Acerca de", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
