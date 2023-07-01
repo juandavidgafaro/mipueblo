a97ff35e3f9966943ed3b660eeddc727917d0f39
OrionAlzate <orion_alzate@hotmail.com>
2023-06-30 21:00:11 -0500

    Add: Cambio viernes

--- app/src/main/java/com/example/mipueblo/Home.java
+++ app/src/main/java/com/example/mipueblo/Home.java
@@ -1,6 +1,9 @@
 package com.example.mipueblo;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,6 +13,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.Locale;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -72,22 +76,55 @@ public class Home extends AppCompatActivity {
 
         switch (itemSeleccionado){
             case (R.id.opcion1):
-                Toast.makeText(this, "accionaste Español", Toast.LENGTH_SHORT).show();
+                this.cambiarIdioma("es");
+                Intent intentEspanol = new Intent(Home.this, Home.class);
+                startActivity(intentEspanol);
                 break;
 
             case (R.id.opcion2):
-                Toast.makeText(this, "Accionaste Ingles", Toast.LENGTH_SHORT).show();
+                this.cambiarIdioma("en");
+                Intent intentIngles = new Intent(Home.this, Home.class);
+                startActivity(intentIngles);
                 break;
 
             case (R.id.opcion3):
-                Toast.makeText(this, "Accionaste Italiano", Toast.LENGTH_SHORT).show();
+                this.cambiarIdioma("it");
+                Intent intentItaliano = new Intent(Home.this, Home.class);
+                startActivity(intentItaliano);
                 break;
-
             case (R.id.opcion4):
                 Toast.makeText(this, "Accionaste Acerca de", Toast.LENGTH_SHORT).show();
                 break;
         }
         return super.onOptionsItemSelected(item);
     }
+
+    // metodo para cambiar el idioma de la app
+
+    public void cambiarIdioma(String idioma){
+
+        // Crear un objeto de la clase LOCAL para cambiar el idioma
+        Locale locale  = new Locale(idioma);
+
+        // Establecemos la configuracion del idioma en el celular
+        Resources resources = getResources();
+        Configuration configuration = resources.getConfiguration();
+        configuration.setLocale(locale);
+
+        // actualizar la configuracion del celular
+        /*Context context = getBaseContext();
+        context = context.createConfigurationContext(configuration);
+        Resources resourcesActualizado = context.getResources();
+        resourcesActualizado.updateConfiguration(configuration, resourcesActualizado.getDisplayMetrics());*/
+
+        getBaseContext().
+                getResources().updateConfiguration(configuration, getBaseContext()
+                        .getResources()
+                        .getDisplayMetrics());
+
+    }
+
+
+
 
 }
